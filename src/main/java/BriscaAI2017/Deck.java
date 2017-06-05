d02a9ef42512ea307c9360138e1a357c59472137
Iroxsmyth <victor.lugo3@upr.edu>
2017-06-05 12:59:10 -0700

    First Commit: Main page

--- src/main/java/BriscaAI2017/Deck.java
+++ src/main/java/BriscaAI2017/Deck.java
@@ -1,7 +1,61 @@
 package BriscaAI2017;
 
+import java.util.ArrayList;
+
 /**
  * Created by vikta on 6/3/2017.
  */
 public class Deck {
+
+    private ArrayList<String> suites = new ArrayList<String>();
+
+    private ArrayList<Card> cardList;
+
+    public Deck(){
+        suites.add("Espada");
+        suites.add("Bastón");
+        suites.add("Moneda");
+        suites.add("Copa");
+        for(int i = 0; i < 4 ; i++) {
+            for (int j = 1; j < 11; j++) {
+                if (j == 1) {
+                    cardList.add(new Card(suites.get(i), j, 11));
+                } else if (j == 3) {
+                    cardList.add(new Card(suites.get(i), j, 10));
+                } else if (j == 8) {
+                    cardList.add(new Card(suites.get(i), 10, 2));
+                } else if (j == 9) {
+                    cardList.add(new Card(suites.get(i), 11, 3));
+                } else if (j == 10) {
+                    cardList.add(new Card(suites.get(i), 12, 4));
+                } else {
+                    cardList.add(new Card(suites.get(i), j, 0));
+                }
+            }
+        }
+    }
+
+    public Card getTopCard(){
+        return cardList.remove(cardList.size()-1);
+    }
+
+    public void shuffle(){
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < cardList.size(); j++){
+                Card temp = cardList.remove(0);
+                cardList.add((int) (Math.random() * (39 - 0 +  1))+0,temp);
+            }
+        }
+    }
+
+    public boolean empty(){
+        return cardList.isEmpty();
+    }
+
+    public Card trump(){
+        Card temp = cardList.remove(cardList.size());
+        cardList.add(0,temp);
+        return temp;
+    }
+
 }
--- src/main/java/BriscaAI2017/Rules.java
+++ src/main/java/BriscaAI2017/Rules.java
@@ -4,4 +4,32 @@ package BriscaAI2017;
  * Created by vikta on 6/3/2017.
  */
 public class Rules {
+
+    private Card trump;
+
+    public Rules(Card killer){
+        this.trump = killer;
+    }
+
+    public int winnerOfRound(ArrayList<Card> round){
+        return 0;
+    }
+
+    public int winnerOfGame(ArrayList<Card> player1, ArrayList<Card> player2){
+        int score1 = 0;
+        int score2 = 0;
+        for(int i = 0; i < player1.size(); i++){
+            score1 = score1 + player1.get(i).getValue();
+        }
+        for(int j = 0; j < player2.size(); j++){
+            score2 = score2 + player2.get(j).getValue();
+        }
+        if(score1 > score2){
+            return 1;
+        }
+        else if(score1 < score2){
+            return 2;
+        }
+        else return 0;
+    }
 }
